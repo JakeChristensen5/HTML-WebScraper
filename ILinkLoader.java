bacc0fd5b89617b91d38438dc8be157a36f99542
Jake Christensen <83853752+JakeChristensen5@users.noreply.github.com>
2022-05-25 18:19:41 -0500

    Update ILinkLoader.java

--- ILinkLoader.java
+++ ILinkLoader.java
@@ -1,11 +1,4 @@
-// --== CS400 File Header Information ==--
-// Individual Code: Cooler WikiGame Project 3, red group DE team
-// Name: Jake Christensen
-// Email: jrchristens2@wisc.edu
-// Team: DE
-// TA: April
-// Lecturer: Florian
-// Notes to Grader: Made some interface changes and addressed interface changes with group to keep them up to date.
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
