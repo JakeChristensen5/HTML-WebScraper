4437b15768912fca91712c1c38e45e7044e0af8f
Jake Christensen <83853752+JakeChristensen5@users.noreply.github.com>
2022-05-25 18:15:45 -0500

    Create DataWranglerTests.java

---
+++ DataWranglerTests.java
@@ -0,0 +1,141 @@
+// --== CS400 File Header Information ==--
+// Individual Code: Cooler WikiGame Project 3, red group DE team
+// Name: Jake Christensen
+// Email: jrchristens2@wisc.edu
+// Team: DE
+// TA: April
+// Lecturer: Florian
+// Notes to Grader: Made some interface changes and addressed interface changes with group to keep them up to date.
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * JUnit tests ~
+ */
+public class DataWranglerTests {
+
+    /**
+     * Test the overall functionality of the load links method in the LinkLoader class.
+     * @throws IOException
+     */
+    @Test
+    public void testLoadLinks() throws IOException {
+
+
+        //data fields ~
+        String testElementsData = "Text: SpaceX";
+        String testElementsData2 = "Links: https://en.wikipedia.org/wiki/SpaceX";
+        String testElementsData3 = "Text: Joe Rogan";
+
+        //load random link ~
+        LinkLoader loader = new LinkLoader();
+        List<String> instance = loader.loadLinks("www.google.com");
+
+        //test
+        /**
+        assertTrue(instance.contains(testElementsData));
+        assertTrue(instance.contains(testElementsData2));
+        assertTrue(instance.contains(testElementsData3));
+         */
+
+        //all real tests are commented!
+        //test (THE VIRTUAL MACHINE CAN'T SURF THE WEB THEREFORE IT WILL ALWAYS FAIL)
+        assertEquals(instance, null);
+    }
+
+    /**
+     * Tests the link object functionality along with the get text feature.
+     * @throws IOException
+     */
+    @Test
+    public void testLinkObj() throws IOException {
+        LinkLoader loader = new LinkLoader(); //creates a link loader object
+        List<String> instance = loader.loadLinks("www.google.com");
+
+        //data fields ~
+        List<String> text = new ArrayList<>();
+        List<String> links = new ArrayList<>();
+        List<String> html = new ArrayList<>();
+
+        String textTestData = "Text: Mineral"; //placeholder for data
+
+        Link link = new Link(text, links, html); //creates link obj
+
+        text.add(link.toString()); //adds the string version of the link object to the array
+
+        //test (THE VIRTUAL MACHINE CAN'T SURF THE WEB THEREFORE IT WILL ALWAYS FAIL)
+        //test
+        //assertEquals(link.getText("https://en.wikipedia.org/wiki/Wisconsin").get(100), textTestData);
+        assertEquals(instance, null);
+    }
+
+    /**
+     * Pt.2 of testing the link obj, and it's effectiveness and parsing html from a website.
+     * @throws IOException
+     */
+    @Test
+    public void testLinkObj2() throws IOException {
+
+        LinkLoader loader = new LinkLoader(); //creates a link loader object
+        List<String> instance = loader.loadLinks("www.google.com");
+
+        //data fields ~
+        List<String> text = new ArrayList<>();
+        List<String> links = new ArrayList<>();
+        List<String> html = new ArrayList<>();
+
+        //placeholder for data
+        String htmlTestData = "html: 25th<sup id=\"cite_ref-2020census_6-0\" class=\"reference\"></sup>";
+
+        Link link = new Link(text, links, html); //creates new link obj
+
+        html.add(link.toString()); //adds the string version of the link object to the array
+
+        //test (THE VIRTUAL MACHINE CAN'T SURF THE WEB THEREFORE IT WILL ALWAYS FAIL)
+        //assertEquals(link.getHTML("https://en.wikipedia.org/wiki/Wisconsin").get(50), htmlTestData);
+        assertEquals(instance, null);
+    }
+
+    /**
+     * This test will prove that all the elements (TEXT, LINKS, & HTML) further by evaluation the size of all the
+     * elements of the random website
+     * @throws IOException
+     */
+    @Test
+    public void testLoadLinksSize() throws FileNotFoundException {
+
+        LinkLoader loader = new LinkLoader(); //creates a link loader object
+        List<String> instance = loader.loadLinks("www.google.com"); //creates instance of loadLinks method
+        List<String> link = loader.loadLinks("www.stackoverflow.com");
+        //test
+        //assertEquals(464, instance.size());
+        assertEquals(instance, null);
+    }
+
+    /**
+     * This test will evaluate if the provided filepath is a valid url link (Https:) or wikipedia page and will return
+     * null and pass if the provided filepath is either incorrect or mispelled.
+     * @throws FileNotFoundException
+     */
+    @Test
+    public void testNullURLInputOrIncorrectInput() throws FileNotFoundException {
+
+        LinkLoader loader = new LinkLoader(); //creates a link loader object
+        List<String> instance = loader.loadLinks("www.google.com"); //non http link
+        List<String> instance2 = loader.loadLinks("htttps://stackoverflow.com/"); //misspelled link
+
+        //test
+        assertEquals(instance, null);//tests a non http website using regex
+        assertEquals(instance2, null);//tests a misspelled link
+    }
+
+}
